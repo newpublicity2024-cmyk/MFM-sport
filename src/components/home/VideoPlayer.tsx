1a1f16691a41cd41429744c4b2a7e86952c724d8
yahia bencheddi <newpublicity2024@gmail.com>
2026-05-13 12:00:14 +0100

    fix(home): use aspect-video on VideoPlayer wrapper for content-driven sizing

--- src/components/home/VideoPlayer.tsx
+++ src/components/home/VideoPlayer.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export function VideoPlayer({ videoId, title }: Props) {
   return (
-    <div className="relative h-full w-full overflow-hidden rounded-2xl bg-black">
+    <div className="relative aspect-video w-full overflow-hidden rounded-2xl bg-black">
       <iframe
         key={videoId}
         src={`https://www.youtube.com/embed/${videoId}`}
